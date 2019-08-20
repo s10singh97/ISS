494f4f67c1a6ee80319ed842d13f73d6c326dbbd
s10singh97 <s10singh97@yahoo.in>
2019-08-20 12:36:40 +0530

    hill cipher

---
+++ Aug20/hillcipher/1.cpp
@@ -0,0 +1,115 @@
+// Hill cipher
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int** multiply(int m1, int m2, int mat1[][2], int n1, int n2, int mat2[][2]) 
+{ 
+    int x, i, j;
+    int **res = new int *[m1];
+    for (i = 0; i < m1; i++)  
+    { 
+        res[i] = new int [n2];
+        for (j = 0; j < n2; j++)  
+        { 
+            res[i][j] = 0; 
+            for (x = 0; x < m2; x++)  
+            { 
+                *(*(res + i) + j) += *(*(mat1 + i) + x) * *(*(mat2 + x) + j); 
+            } 
+        } 
+    }
+    return res;
+} 
+
+void print(int m1, int m2, int **res)
+{
+    for(int i = 0; i < m1; i++)
+    {
+        for(int j = 0; j < m2; j++)
+            cout<<res[i][j]<<" ";
+        cout<<"\n";
+    }
+}
+
+int **mmod(int m1, int m2, int **ct)
+{
+    for(int i = 0; i < m1; i++)
+        for(int j = 0; j < m2 ; j++)
+            ct[i][j] = ct[i][j] % 26;
+    return ct;
+}
+
+void p_to_c(string file, string ofile, int key[2][2])
+{
+    ifstream ip(file.c_str());
+    ofstream of(ofile.c_str());
+    string a;
+    while(!ip.eof())
+        getline(ip, a);
+    int pt[1][2];
+    string res;
+    for(int i = 0; i < a.length(); i += 2)
+    {
+        pt[0][0] = int(a[i]-'a');
+        pt[0][1] = int(a[i+1]-'a');
+        int **ct = multiply(1, 2, pt, 2, 2, key);
+        ct = mmod(1, 2, ct);
+        for(int i = 0; i < 2; i++)
+            res.push_back(char(toupper('a'+ct[0][i])));
+    }
+    of<<res;
+}
+
+void c_to_p(string file, string ofile, int key[2][2])
+{
+    ifstream ip(file.c_str());
+    ofstream of(ofile.c_str());
+
+    while(!ip.eof())
+    {
+        string a;
+        getline(ip, a);
+        
+
+
+        
+    }
+}
+
+int main()
+{
+    cout<<"Enter Key: ";
+    int key[2][2];
+    for(int i = 0; i < 2; i++)
+        for(int j = 0; j < 2; j++)
+            cin>>key[i][j];
+    cout<<"Choose the following: \n1. Encrypt a file\n2. Decrypt a file\n";
+    int ch;
+    cin>>ch;
+    string ofile, file;
+    char c = 'y';
+    do{
+        switch(ch)
+        {
+            case 1: cout<<"name the file to encrpyt: ";
+                    cin>>file;
+                    cout<<"name the output file: ";
+                    cin>>ofile;
+                    p_to_c(file, ofile, key);
+                    break;
+
+            case 2: cout<<"name the file to decrpyt: ";
+                    cin>>file;
+                    cout<<"name the output file: ";
+                    cin>>ofile;
+                    c_to_p(file, ofile, key);
+                    break;
+
+            default:cout<<"Enter correct choice\n";
+        }
+        cout<<"Do you want to try again[y/N]: ";
+            cin>>c;
+    }while(ch == 'y');
+    return 0;
+}
