fee0b61330eb9e20cb3f3569871be5551e955bb7
s10singh97 <s10singh97@yahoo.in>
2019-09-09 18:02:21 +0530

    rail fence

--- Aug27/Rail Fence/1.cpp
+++ Aug27/Rail Fence/1.cpp
@@ -9,10 +9,29 @@ bool isValid(string key)
     sort(key.begin(), key.end());
     for(int i = 0; i < key.length(); i++)
     {
-        if(key[i] != '0'+i)
+        if(key[i] != '1'+i)
             return false;
     }
     return true;
+}
+
+void print(char **mat, int row, int col)
+{
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+            cout<<mat[i][j]<<"\t";
+        cout<<"\n";
+    }
+}
+
+string removespace(string a)
+{
+    string b;
+    for(int i = 0; i < a.length(); i++)
+        if(isalpha(a[i]))
+            b.push_back(a[i]);
+    return b;
 }
 
 void p_to_c(string file, string ofile, string key)
@@ -28,11 +47,36 @@ void p_to_c(string file, string ofile, string key)
     while(!ip.eof())
         getline(ip, a);
     
-    string res;
-
-    for(int i = 0, j = 0; i < a.length(); i++)
+    string a_mod = removespace(a);
+    char **mat = new char *[key.length()];
+    int i = 0, k = 0;
+    int a_l = ceil((float)a_mod.length()/key.length());
+    while(i < a_l)    
     {
-        
+        mat[i] = new char [key.length()];
+        for(int j = 0; j < key.length(); j++)
+        {
+            if(k < a_mod.length())
+                mat[i][j] = a_mod[k++];
+            else
+                mat[i][j] = char('a'+ (k++ % 26));
+        }
+        i++;
+    }
+    // print(mat, i, key.length());
+
+    string res;
+    k = 0;
+    while(k != key.length())
+    {
+        int temp = 0;
+        while(k != key[temp]-'0'-1)
+            temp++;
+        for(i = 0; i < a_l; i++)
+        {
+            res.push_back(toupper(mat[i][temp]));
+        }
+        k++;
     }
     of<<res;
 }
@@ -44,12 +88,31 @@ void c_to_p(string file, string ofile, string key)
     string a;
     while(!ip.eof())
         getline(ip, a);
-    string kk;
     
-    string res;
-    for(int i = 0, j = 0; i < a.length(); i++)
+    char **mat = new char *[key.length()];
+    int i = 0, k = 0;
+    int a_l = ceil((float)a.length()/key.length());
+    while(i < key.length())    
     {
-        
+        mat[i] = new char [a_l];
+        for(int j = 0; j < a_l; j++)
+        {
+            if(k < a.length())
+                mat[i][j] = tolower(a[k++]);
+            else
+                mat[i][j] = char('a'+ (k++ % 26));
+        }
+        i++;
+    }
+    // print(mat, i, a_l);
+
+    string res;
+    k = 0, i = 0; int j = 0;
+    while(j != a_l)
+    {
+        for(i = 0; i < key.length(); i++)
+            res.push_back(mat[key[i]-'0'-1][j]);
+        j++;
     }
     of<<res;
 }
